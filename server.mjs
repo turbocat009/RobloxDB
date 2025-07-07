023dc613de05ab87494d1608a33549b9c68984ae
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-07-07 15:51:57 +0200

    first commit

---
+++ server.mjs
@@ -0,0 +1,35 @@
+import http from 'http';
+import { URL } from 'url';
+
+import database from './database.json' with { type: 'json' };
+
+const server = http.createServer((req, res) => {
+    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+    const params = Object.fromEntries(reqUrl.searchParams.entries());
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    
+
+    if (Object.keys(params).length === 0) {
+        res.end(JSON.stringify({ error: 'No parameters provided' }));
+    } 
+    
+    else{
+        if (!params.Username || !params.Password) {
+            res.end(JSON.stringify({ error: 'Username and Password required' }));
+            return;
+        }
+        const user = database.find(user => user.Username === params.Username && user.Password === params.Password);
+        if (!user) {
+            res.end(JSON.stringify({ error: 'Invalid Username or Password' }));
+            return;
+        }
+        const UserData = database.find(user => user.Username === params.Username);
+        res.end(JSON.stringify(UserData));
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
